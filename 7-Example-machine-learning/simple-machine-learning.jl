c250eeef1d06347c9b7662628ff37beb481d3740
Nir Shaviv <76590909+nshaviv@users.noreply.github.com>
2026-05-13 11:47:23 +0300

    Activate local environment and add dependencies

--- 7-Example-machine-learning/simple-machine-learning.jl
+++ 7-Example-machine-learning/simple-machine-learning.jl
@@ -1,8 +1,8 @@
 cd("7-Example-machine-learning")
-using Pkg; Pkg.activate(".") # I had a clash between Flux and another package I used. The easiest was to activate a local environment  
-#Pkg.add("Flux")
-#Pkg.add("Plots")
-#Pkg.add("ProgressMeter")
+using Pkg; Pkg.activate(".")  
+Pkg.add("Flux")
+Pkg.add("Plots")
+Pkg.add("ProgressMeter")
 
 using Flux
 using ProgressMeter 
